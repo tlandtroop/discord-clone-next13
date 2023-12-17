ae680ddd21130e6fa8c4010c52e9ed81dee51e89
Tyler Landtroop <tlandtroop@ufl.edu>
2023-12-17 17:54:42 -0500

    added channel creation modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -61,7 +61,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem 
+          <DropdownMenuItem
             onClick={() => onOpen("members", { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
@@ -70,7 +70,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel")}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
