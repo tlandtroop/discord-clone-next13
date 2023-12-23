5848c5c6f16d411e00de80d62affe91808ca1748
Tyler Landtroop <tlandtroop@ufl.edu>
2023-12-23 17:08:19 -0600

    fixing errors

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -11,7 +11,7 @@ const SetupPage = async () => {
     where: {
       members: {
         some: {
-          profileId: profile.id,
+          profileId: (profile as any).id,
         },
       },
     },
